0f52174a6dc6487f90c489f72a1f6bd026056b2b
Rajarshi Yelamati <37547796+rajarshiy@users.noreply.github.com>
2021-08-27 13:19:24 -0400

    Add files via upload

---
+++ cerner.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+    window['CernerSmartEmbeddableLib'].calcFrameHeight = function () {
+        return 1000;
+    };
+    console.log(window['CernerSmartEmbeddableLib']);
+}, 3000)
