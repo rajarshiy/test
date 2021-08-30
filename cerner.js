2e2be81012d79b17c236f46202205b6d91cfa058
Rajarshi Yelamati <37547796+rajarshiy@users.noreply.github.com>
2021-08-30 11:58:22 -0400

    Update cerner.js

--- cerner.js
+++ cerner.js
@@ -1,7 +1,5 @@
-setTimeout(() => {
     window['CernerSmartEmbeddableLib'].calcFrameHeight = function () {
         return 1000;
     };
     alert(JSON.stringify(window['CernerSmartEmbeddableLib'].calcFrameHeight));
     console.log(window['CernerSmartEmbeddableLib']);
-}, 3000)
